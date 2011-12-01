53c8956ae00160f09c1729d186b430d9823751c6
Vasiliy Tolstov <vase@clodo.ru>
2011-12-01 13:10:50 +0400

    add immediate reload
    Signed-off-by: Vasiliy Tolstov <vase@clodo.ru>

--- cookbooks/php/definitions/apache_php_value.rb
+++ cookbooks/php/definitions/apache_php_value.rb
@@ -5,12 +5,12 @@ define :apache_php_value  do
   if params[:value] != ""
     execute "mod php_value #{params[:name]}" do
       command "sed -i 's/#{params[:name]}=(.*)/#{params[:name]}=#{params[:value]}/g' #{node[:php][:ext_conf_dir]}/preset.ini"
-      notifies :reload, resources(:service => "apache2")
+      notifies :reload, resources(:service => "apache2"), :immediately
       only_if do ::File.exists?("#{node[:php][:ext_conf_dir]}/preset.ini") end
     end
     execute "add php_value #{params[:name]}" do
       command "echo '#{params[:name]}=#{params[:value]}' >> #{node[:php][:ext_conf_dir]}/preset.ini"
-      notifies :reload, resources(:service => "apache2")
+      notifies :reload, resources(:service => "apache2"), :immediately
       not_if do ::File.exists?("#{node[:php][:ext_conf_dir]}/preset.ini") end
     end
   end
