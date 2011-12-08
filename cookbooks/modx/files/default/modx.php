7ab39cc14c6eba44032117918985df8f2d745a37
Vasiliy Tolstov <vase@clodo.ru>
2011-12-08 13:40:18 +0400

    fix first get
    Signed-off-by: Vasiliy Tolstov <vase@clodo.ru>

--- cookbooks/modx/files/default/modx.php
+++ cookbooks/modx/files/default/modx.php
@@ -29,7 +29,7 @@ var_dump($ARGS);
 $ARG_COOKIE = "cookie.txt";
 
 $ch = curl_init();
-curl_setopt ($ch, CURLOPT_URL, $ARGS['domain'] . "/setup/index.php?s=set");
+curl_setopt ($ch, CURLOPT_URL, $ARGS['domain'] . "/setup/");
 curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
 curl_setopt ($ch, CURLOPT_TIMEOUT, 60);
