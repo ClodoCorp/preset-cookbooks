9b3b52cd93b3f34d94c6da4eb1b29c1430400966
Vasiliy Tolstov <v.tolstov@selfip.ru>
2011-12-14 13:49:58 +0300

    fix stale files
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- cookbooks/drupal/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-
-#::Chef::Node.send(:include, Opscode::OpenSSL::Password)
-
-#node['web_app']['system']['pass'] = secure_password
-
--- cookbooks/wordpress/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-::Chef::Node.send(:include, Opscode::OpenSSL::Password)
-
-default['wordpress']['keys']['auth'] = secure_password
-default['wordpress']['keys']['secure_auth'] = secure_password
-default['wordpress']['keys']['logged_in'] = secure_password
-default['wordpress']['keys']['nonce'] = secure_password
-
