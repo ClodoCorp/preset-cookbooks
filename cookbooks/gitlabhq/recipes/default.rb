5ec484421eeb898e3a59049ae24d657f9d1d2b84
Vasiliy Tolstov <v.tolstov@selfip.ru>
2011-12-26 15:36:13 +0400

    add thin server
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- cookbooks/gitlabhq/recipes/default.rb
+++ cookbooks/gitlabhq/recipes/default.rb
@@ -7,17 +7,6 @@ ruby_block "reset group list" do
   action :nothing
 end
 
-user "gitlabhq" do
-  uid 600
-  gid "root"
-  shell "/bin/bash"
-  password `openssl passwd -1 #{node['system']['pass']}`
-  comment "GitLabHQ Admin User"
-  supports :manage_home => true
-  home "/home/gitlabhq"
-  notifies :create, "ruby_block[reset group list]", :immediately
-end
-
 user "git" do
   uid 601
   gid "root"
@@ -29,8 +18,18 @@ user "git" do
   notifies :create, "ruby_block[reset group list]", :immediately
 end
 
+user "gitlabhq" do
+  uid 600
+  gid "root, git"
+  shell "/bin/bash"
+  password `openssl passwd -1 #{node['system']['pass']}`
+  comment "GitLabHQ Admin User"
+  supports :manage_home => true
+  home "/home/gitlabhq"
+  notifies :create, "ruby_block[reset group list]", :immediately
+end
 
-%w{python-dev python-pip git-core git-svn sendmail zlib1g ssh openssl git-core wget curl gcc checkinstall libxml2-dev libxslt-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev libc6-dev libssl-dev libmysql++-dev make build-essential zlib1g-dev postfix ruby1.9 openssh-server rubygems libdaemons-ruby}.each do |pkg|
+%w{gitolite python-dev python-pip git-core git-svn sendmail zlib1g ssh openssl git-core wget curl gcc checkinstall libxml2-dev libxslt-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev libc6-dev libssl-dev libmysql++-dev make build-essential zlib1g-dev postfix ruby1.9 openssh-server rubygems libdaemons-ruby nginx}.each do |pkg|
   package "#{pkg}"
 end
 
@@ -81,7 +80,7 @@ execute "owner" do
 end
 
 execute "mode" do
-  command "chmod -R g+rwX /home/git/repositories/"
+  command "chmod -R g+rwX /home/git/repositories/; chmod g+r /home/git"
 end
 
 REPO_UMASK = 0007;
@@ -95,7 +94,7 @@ execute "python-modules" do
 end
 
 execute "ruby-modules" do
-  command "gem install bundler"
+  command "gem install rails bundler --include-dependencies"
 end
 
 git "#{node['web_app']['system']['dir']}" do
@@ -120,5 +119,44 @@ execute "rake-seed_fu" do
   user "gitlabhq"
   group "gitlabhq"
   command "bundle exec rake db:seed_fu RAILS_ENV=production"
+end
+
+execute "thin" do
+  command "thin install"
+end
+
+service "thin" do
+  start_command "/usr/sbin/invoke-rc.d thin start && sleep 1"
+  stop_command "/usr/sbin/invoke-rc.d thin stop && sleep 1"
+  restart_command "/usr/sbin/invoke-rc.d thin restart && sleep 1"
+  reload_command "/usr/sbin/invoke-rc.d thin reload && sleep 1"
+  supports value_for_platform(
+    "default" => { "default" => [:restart, :reload ] }
+  )
+  action :start
+end
+
+
+file "/etc/thin/gitlabhq.yml" do
+  content "
+user: gitlabhq
+group: gitlabhq
+chdir: /home/gitlabhq/gitlabhq/
+log: log/gitlabhq.thin.log
+socket: /tmp/gitlab.sock
+pid: tmp/pids/gitlab.pid
+
+environment: production
+timeout: 30
+max_conns: 1024
+
+max_persistent_conns: 512
+no-epoll: true
+servers: 1
+daemonize: 1"
+end
+
+execute "Gemfile" do
+  command "sed -i 's///g' /home/gitlab/gitlabhq/Gemfile"
 end
 
