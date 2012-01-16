b3c532fb6be5fcfed8f0b995ca0430fca9d9731e
Vasiliy Tolstov <v.tolstov@selfip.ru>
2012-01-16 16:33:56 +0400

    correcting clean recipes
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- cookbooks/wordpress/recipes/clean.rb
+++ cookbooks/wordpress/recipes/clean.rb
@@ -1,8 +1,8 @@
 include_recipe "chef"
 
-pkgs = %{ chef libjson-ruby1.8 libnet-ssh-gateway-ruby1.8 libmixlib-authentication-ruby1.8 ohai libtreetop-ruby libmixlib-log-ruby1.8 rubygems libnet-ssh2-ruby1.8 libpolyglot-ruby libmoneta-ruby libmime-types-ruby librestclient-ruby1.8 libbunny-ruby ruby1.8 ruby librestclient-ruby liberubis-ruby1.8 libsystemu-ruby1.8 libjson-ruby libmixlib-config-ruby libohai-ruby libuuidtools-ruby1.8 libhighline-ruby1.8 libabstract-ruby1.8 libmixlib-authentication-ruby libmixlib-config-ruby1.8 libnet-ssh-multi-ruby libmixlib-log-ruby libuuidtools-ruby libnet-ssh2-ruby libnet-ssh-multi-ruby1.8 libhighline-ruby rubygems1.8 libbunny-ruby1.8 libmixlib-cli-ruby libruby1.8 libmixlib-cli-ruby1.8 libyajl-ruby libmoneta-ruby1.8 libohai-ruby1.8 liberubis-ruby }
+pkgs = %w{ chef libjson-ruby1.8 libnet-ssh-gateway-ruby1.8 libmixlib-authentication-ruby1.8 ohai libtreetop-ruby libmixlib-log-ruby1.8 rubygems libnet-ssh2-ruby1.8 libpolyglot-ruby libmoneta-ruby libmime-types-ruby librestclient-ruby1.8 libbunny-ruby ruby1.8 ruby librestclient-ruby liberubis-ruby1.8 libsystemu-ruby1.8 libjson-ruby libmixlib-config-ruby libohai-ruby libuuidtools-ruby1.8 libhighline-ruby1.8 libabstract-ruby1.8 libmixlib-authentication-ruby libmixlib-config-ruby1.8 libnet-ssh-multi-ruby libmixlib-log-ruby libuuidtools-ruby libnet-ssh2-ruby libnet-ssh-multi-ruby1.8 libhighline-ruby rubygems1.8 libbunny-ruby1.8 libmixlib-cli-ruby libruby1.8 libmixlib-cli-ruby1.8 libyajl-ruby libmoneta-ruby1.8 libohai-ruby1.8 liberubis-ruby }
 
-cmds = %{ "rm -rf /var/chef" }
+cmds = %w{ "rm -rf /var/chef" }
 
 chef_purge do
   clean true
