838b4ca0f91c14c4f57148af4fc917da764861ab
Vasiliy Tolstov <v.tolstov@selfip.ru>
2012-01-17 17:46:37 +0400

    update ajenti preset
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- cookbooks/ajenti/recipes/default.rb
+++ cookbooks/ajenti/recipes/default.rb
@@ -3,3 +3,8 @@ include_recipe value_for_platform(
     [ "centos", "redhat", "suse", "fedora" ] => { "default" => "ajenti::centos" },
     [ "debian", "ubuntu" ] => { "default" => "ajenti::debian" })
 
+service "ajenti" do
+  supports :start => true, :stop => true, :restart => true
+  action :nothing
+end
+
---
+++ cookbooks/ajenti/recipes/setup.rb
@@ -0,0 +1,34 @@
+
+require 'base64'
+require 'digest/sha1'
+node['web_app']['ui']['passhash'] = Base64.encode64(Digest::SHA1.digest("#{node['web_app']['ui']['pass']}"))
+
+
+%w{python-psutil python-imaging}.each do |pkg|
+  package "#{pkg}" do
+    action :install
+  end
+end
+
+%w{webserver_common power services hosts network dns taskmgr terminal users iptables shell fm filesystems notepad cron logs sysload pkgman}.each do |pkg|
+  execute "get #{pkg}" do
+    command "/usr/bin/ajenti-pkg get #{pkg}"
+  end
+end
+
+
+execute "remove admin user" do
+  command "sed '/^\[users\]/,+2 d' -i /etc/ajenti/ajenti.conf"
+end
+
+template "/etc/ajenti/ajenti.conf" do
+  source "ajenti.conf"
+  mode 0600
+  backup 0
+  variables (:login => node[:web_app][:ui][:login], :password => node['web_app']['ui']['passhash'])
+end
+
+service "ajenti" do
+  action :restart
+end
+
