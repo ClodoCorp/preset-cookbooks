77768e664232166bf2d1a2b124403a2c9fc6a017
Alibek Amaev <rnz@clodo.ru>
2012-01-20 15:07:38 +0400

    fix add user

--- cookbooks/ejabberd/recipes/default.rb
+++ cookbooks/ejabberd/recipes/default.rb
@@ -15,10 +15,31 @@ template "/etc/ejabberd/ejabberd.cfg" do
   action :create
 end
 
-execute "add ejabberd admin user" do
-  command "ejabberdctl register #{node[:web_app][:ui][:login]} #{node[:web_app][:ui][:domain]} #{node[:web_app][:ui][:pass]}"
-end
-
 service "ejabberd" do
   action :start
 end
+
+ruby_block "Add ejabberd user" do
+  block do
+    # TODO: Check return value and use nicer system call    
+    ret = 111
+    until ret != 0
+        mon=`ejabberdctl status 1>/dev/null;`
+	ret = $?
+	sleep 1
+    end
+    system("ejabberdctl register #{node[:web_app][:ui][:login]} #{node[:web_app][:ui][:domain]} #{node[:web_app][:ui][:pass]}")
+    ret =$?
+    if ret == 0 then
+      Chef::Log.info("Add ejabberd user #{node[:web_app][:ui][:login]} successfuly.")
+    else
+      Chef::Log.info("Add ejabberd user #{node[:web_app][:ui][:login]} fail!")
+    end
+  end
+  action :create
+end
+
+#execute "add ejabberd admin user" do
+#  command "ejabberdctl register #{node[:web_app][:ui][:login]} #{node[:web_app][:ui][:domain]} #{node[:web_app][:ui][:pass]}"
+#end
+
