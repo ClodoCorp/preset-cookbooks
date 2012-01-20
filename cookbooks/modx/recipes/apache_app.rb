cb551faf1798817dccd10cbe026ace48e3351f12
Vasiliy Tolstov <vase@selfip.ru>
2012-01-20 11:11:27 +0400

    update modx recipe, delete php install
    Signed-off-by: Vasiliy Tolstov <vase@selfip.ru>

--- cookbooks/modx/recipes/apache_app.rb
+++ cookbooks/modx/recipes/apache_app.rb
@@ -6,13 +6,15 @@ apache_site "000-default" do
   enable false
 end
 
-apache_app "modx" do
-  template "modx.conf.erb"
+apache_app "#{node['web_app']['ui']['domain']}" do
+  template "#{node['web_app']['system']['name']}.conf.erb"
   docroot "#{node['web_app']['system']['dir']}"
   server_name "#{node['hostname']}.clodo.ru"
   server_aliases "#{node['web_app']['ui']['domain']} www.#{node['web_app']['ui']['domain']}"
 end
 
+
 apache_php_value "date.timezone" do
   value "Europe/Moscow"
 end
+
--- cookbooks/modx/recipes/setup.rb
+++ cookbooks/modx/recipes/setup.rb
@@ -1,26 +1,80 @@
 include_recipe "modx"
-include_recipe "hosts"
+include_recipe "chef::depends"
 
-hosts "127.0.0.1" do
-  action "add"
-  host "#{node['web_app']['ui']['domain']}"
+ruby_block "setup" do
+  block do
+    Gem.clear_paths
+    require 'curb'
+
+    timeout = 20
+    host = "localhost:80"
+    real_host = "#{node['web_app']['ui']['domain']}"
+    Chef::Log.info "call get on #{host}, maximal request time: #{timeout} seconds"
+    c = Curl::Easy.new() do |curl|
+      curl.url = "http://#{host}/setup/"
+      curl.headers['Host'] = real_host
+      curl.verbose = true
+      curl.follow_location = true
+      curl.enable_cookies = true
+      curl.cookiefile = "/tmp/cookie.txt"
+      curl.timeout = timeout
+    end
+    c.perform
+    if c.response_code == 200
+      Chef::Log.info "GET success!"
+      c.url = "http://#{host}/setup/index.php?s=set"
+      c.http_get
+      Chef::Log.info "get1: #{c.body_str}"
+
+      c.url = "http://#{host}/setup/?"
+      c.http_post("language=ru", "proceed=Select")
+      c.perform
+      sleep(2)
+      Chef::Log.info "get2: #{c.body_str}"
+      if c.response_code == 200
+        Chef::Log.info "Step 1 success!"
+	c.url = "http://#{host}/setup/?action=welcome"
+	c.http_post("proceed=Далее")
+	c.perform
+	c.url = "http://#{host}//setup/?action=options"
+	c.http_post("installmode=0", "new_folder_permissions=0755", "new_file_permissions=0644", "unpacked=1", "proceed=Далее")
+	c.perform
+	Chef::Log.info "get3: #{c.body_str}"
+
+        c.url = "http://#{host}/setup/?action=database"
+	c.http_post("database_type=mysql", "database_server=localhost", "database_user=#{node[:web_app][:system][:name]}",
+		    "database_password=#{node[:web_app][:system][:pass]}", "dbase=#{node[:web_app][:system][:name]}",
+		    "table_prefix=pref_", "database_connection_charset=utf8", "database_collation=utf8_general_ci",
+		    "cmsadmin=#{node[:web_app][:ui][:login]}", "cmsadminemail=#{node[:web_app][:ui][:email]}",
+		    "cmspassword=#{node[:web_app][:ui][:pass]}", "cmspasswordconfirm=#{node[:web_app][:ui][:pass]}",
+		    "unpacked=1", "proceed=Далее")
+	c.perform
+	Chef::Log.info "get4: #{c.body_str}"
+	c.url = "http://#{host}/setup/?action=summary"
+	c.http_post("proceed=Установить")
+	c.perform
+	Chef::Log.info "get5: #{c.body_str}"
+	c.url = "http://#{host}/setup/?action=install"
+	c.http_post("proceed=Далее")
+	c.perform
+	Chef::Log.info "get6: #{c.body_str}"
+	c.url = "http://#{host}/setup/?action=complete"
+	c.http_post("cleanup=1", "proceed=Войти")
+	c.perform
+	Chef::Log.info "get7: #{c.body_str}"
+      else
+	 Chef::Log.error "Step 1 failed!"
+      end
+    else
+      Chef::Log.error "GET failed!"
+    end
+    c.close()
+  end
+  action :create
 end
 
-execute "setup modx" do
-  command "php /tmp/modx.php 'login=#{node['web_app']['ui']['login']}' 'pass=#{node['web_app']['ui']['pass']}' 'email=#{node['web_app']['ui']['email']}' 'domain=#{node['web_app']['ui']['domain']}' 'title=#{node['web_app']['ui']['title']}' 'db_name=#{node['web_app']['system']['name']}' 'db_host=localhost' 'db_login=#{node['web_app']['system']['name']}' 'db_pass=#{node['web_app']['system']['pass']}'"
-  action :nothing
+execute "htaccess" do
+  user "www-data"
+  group "www-data"
+  command "mv -f #{node[:web_app][:system][:dir]}/ht.access #{node[:web_app][:system][:dir]}/.htaccess"
 end
-
-remote_file "/tmp/modx.php" do
-  source "modx.php"
-  cookbook "modx"
-  mode 0600
-  backup 0
-  notifies :run, resources(:execute => "setup modx"), :immediately
-end
-
-file "/tmp/modx.php" do
-  action :delete
-  backup 0
-end
-
