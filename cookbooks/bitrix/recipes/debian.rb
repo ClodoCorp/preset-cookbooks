ace4ca4c2da035b447ea4121c084a777b4807497
Vasiliy Tolstov <vase@clodo.ru>
2012-09-18 01:14:27 +0400

    update bitrix recipe
    Signed-off-by: Vasiliy Tolstov <vase@clodo.ru>

--- cookbooks/bitrix/recipes/debian.rb
+++ cookbooks/bitrix/recipes/debian.rb
@@ -1,7 +1,10 @@
 include_recipe "apt"
+include_recipe "mysql::server"
+include_recipe "apache2"
+include_recipe "nginx"
 
-apt_repository "zwas" do
-  uri "http://repos-source.zend.com/zend-server/deb"
+apt_repository "zend" do
+  uri "http://repos.zend.com/zend-server/deb"
   distribution "server"
   components ["non-free"]
   key "http://repos.zend.com/zend.key"
@@ -9,30 +12,63 @@ apt_repository "zwas" do
 end
 
 
-%w{ geoip-bin geoip-database libgeoip-dev autoconf bash bc catdoc csync2 expect gcc gzip apache2 libidn11 libidn11-dev librsync-dev }.each do |pkg|
-  package "#{pkg}"
+%w{zip zem xsl sockets optimizer-plus mysql mcrypt mbstring json gd ftp fileinfo exif curl ctype bz2 bin bcmath}.each do |pkg|
+  package "php-5.3-#{pkg}-zend-server"
 end
 
-%w{ libtasn1-3 libtasn1-3-dev libtasn1-3-bin make memcached libapache2-mod-geoip libapache2-mod-rpaf }.each do |pkg|
-  package "#{pkg}"
+file "/etc/profile.d/zend.sh" do
+  action :create
+  content "export PATH=$PATH:/usr/local/zend/bin\n"
+  mode "0644"
+  owner "root"
+  group "root"
 end
 
-%w{ msmtp mysql-server nginx ntp openssh-server openssh-client php-5.3-memcache-zend-server libpoppler-cil poppler-data poppler-utils }.each do |pkg|
-  package "#{pkg}"
+%w{libapache2-mod-rpaf mysql-server nginx-full ntp openssh-server openssh-client exim4-daemon-light}.each do |pkg|
+  package pkg
 end
 
-%w{ rsync libsqlite3-0 sqlite3 libsqlite3-dev stunnel4 tar xinetd zend-server-ce-php-5.3 }.each do |pkg|
-  package "#{pkg}"
+template "/var/share/local/preseed/exim4-config" do
+  source "exim4-config.seed.erb"
+  mode 0644
+  owner "root"
+  group "root"
 end
 
-%w{ /etc/nginx/bx/conf /etc/nginx/bx/site_ext_enabled /etc/nginx/bx/site_enabled /etc/nginx/bx/site_avaliable /etc/bx_cluster/nodes }.each do |dir|
-  directory "#{dir}" do
-    action :create
-    owner "root"
-    group "root"
-    mode 0755
-    recursive true
+execute "preseed" do
+  command "debconf-set-selections -v /var/share/local/preseed/exim4-config"
+end
+
+directory "/var/www/htdocs" do
+  owner "www-data"
+  group "www-data"
+  mode "0755"
+  recursive true
+end
+
+execute "dpkg-reconfigure" do
+  command "dpkg-reconfigure exim4-config"
+end
+
+nginx_site node['web_app']['ui']['domain'] do
+  source "bitrix.erb"
+  action :enable
+end
+
+apache_site "000-default" do
+  enable false
+end
+
+apache_app node['web_app']['ui']['domain'] do
+  template "#{node['web_app']['system']['name']}.conf.erb"
+  docroot node['web_app']['system']['dir']
+  server_name "#{node['hostname']}.clodo.ru"
+  server_aliases "#{node['web_app']['ui']['domain']} www.#{node['web_app']['ui']['domain']}"
+end
+
+
+%w{autoindex cgi deflate env negotiation reqtimeout setenvif}.each do |mod|
+  apache_module mod do
+    enable false
   end
 end
-
-
